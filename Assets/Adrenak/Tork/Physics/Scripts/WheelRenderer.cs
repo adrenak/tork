1a690a0719005e95310b17bbc309d5f069c2cb6a
Guillermo Gombau <guillermo.gombau@psycle.com>
2019-04-24 17:12:44 +0200

    Fix WheelRenderer angle calculation
    Related #3

--- Assets/Adrenak/Tork/Physics/Scripts/WheelRenderer.cs
+++ Assets/Adrenak/Tork/Physics/Scripts/WheelRenderer.cs
@@ -36,7 +36,7 @@ namespace Adrenak.Tork {
 
 		void SyncRotation() {
 			transform.localEulerAngles = Vector3.zero;
-			angle += (Time.deltaTime * wheel.Velocity.z) / (2 * Mathf.PI * wheel.radius) * 360;
+			angle += (Time.deltaTime * transform.InverseTransformDirection(wheel.Velocity).z) / (2 * Mathf.PI * wheel.radius) * 360;
 			transform.Rotate(new Vector3(0, 1, 0), wheel.steerAngle - transform.localEulerAngles.y);
 			transform.Rotate(new Vector3(1, 0, 0), angle);
 		}
