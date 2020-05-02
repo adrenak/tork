97f4eba7e52b8effff5a2ed14bd0c201b647b21f
Vatsal Ambastha <adrenak_@outlook.com>
2020-05-02 16:16:27 +0530

    feat: Remove input stuff.
    Add Rigidbody accessor to Vehicle class

--- Assets/Tork/Runtime/Input/IVehicleDriver.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Adrenak.Tork {
-	public interface IVehicleDriver  {
-        void RegisterVehicle(Vehicle vehicle);
-        void DeregisterVehicle(Vehicle vehicle);
-        void DriveVehicles();
-	}
-}
---
+++ Assets/Tork/Samples/Scripts/DemoInputSource.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Adrenak.Tork {
+    public class DemoInputSource : MonoBehaviour {
+        [SerializeField] Vehicle m_Vehicle;
+
+        public void SetVehicle(Vehicle vehicle){
+            m_Vehicle = vehicle;
+        }
+
+        void Update(){
+            m_Vehicle.Motor.value = Input.GetAxis("Vertical");
+            m_Vehicle.Steering.value = Input.GetAxis("Horizontal");
+            m_Vehicle.Brake.value = Input.GetKey(KeyCode.Space) ? 1 : 0;
+        }
+    }
+}
