58e944d40d708d8620a9289ebb94a99f27c74169
Vatsal Ambastha <adrenak_@outlook.com>
2020-09-20 13:46:37 +0530

    feat: Support tiled wheels (like BMW?)

--- Assets/Tork/Runtime/Viz/WheelViz.cs
+++ Assets/Tork/Runtime/Viz/WheelViz.cs
@@ -13,11 +13,11 @@ namespace Adrenak.Tork {
             Handles.DrawWireDisc(t.position, t.right, target.radius);
 
             Handles.color = Color.red;
-            var p1 = t.position + t.up * TorkWheel.k_RayStartHeight;
-            var p2 = t.position - t.up * (target.GetRayLen() - TorkWheel.k_RayStartHeight);
+            var p1 = t.position + t.up * TorkWheel.k_ExtraRayLegnth;
+            var p2 = t.position - t.up * (target.RayLength - TorkWheel.k_ExtraRayLegnth);
             Handles.DrawLine(p1, p2);
 
-            var pos = t.position + (-t.up * (target.GetRayLen() - TorkWheel.k_RayStartHeight - target.CompressionDistance - target.radius));
+            var pos = t.position + (-t.up * (target.RayLength - TorkWheel.k_ExtraRayLegnth - target.CompressionDistance - target.radius));
             Handles.DrawWireDisc(pos, t.right, target.radius);
         }
     }
